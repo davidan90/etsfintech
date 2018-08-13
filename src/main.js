4f88fc8802d5bbbbe7a08478d73f32952992248c
dalvarez <dalvarez@ifbext12.bme.com>
2018-08-13 10:45:26 +0200

    add svg icons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
+import * as svgicon from "vue-svgicon";
 import App from "./App.vue";
 import router from "./routes";
 import store from "./store";
 import i18n from "./i18n";
 import api from "./api";
 import utils from "./utils";
+
+Vue.use(svgicon, {
+  classPrefix: "AppIcon-"
+});
 
 new Vue({
   el: "#etsfintechDavid",
